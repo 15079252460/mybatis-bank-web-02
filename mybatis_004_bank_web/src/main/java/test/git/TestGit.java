a99ff89213e607323eba2a5897d3f016b7345199
liangfengcai <1183268720@qq.com>
2022-11-09 18:19:29 +0800

    master commit1

--- mybatis_004_bank_web/src/main/java/test/git/TestGit.java
+++ mybatis_004_bank_web/src/main/java/test/git/TestGit.java
@@ -5,5 +5,6 @@ public class TestGit {
         System.out.println("second commit");
         System.out.println("thrid commit");
         System.out.println("hot-fix");
+        System.out.println("master test");
     }
 }
