49a5d08413f87570858d2c56bebe35671545f2c2
liangfengcai <1183268720@qq.com>
2022-11-09 18:11:25 +0800

    hot-fix commit1

--- mybatis_004_bank_web/src/main/java/test/git/TestGit.java
+++ mybatis_004_bank_web/src/main/java/test/git/TestGit.java
@@ -4,5 +4,6 @@ public class TestGit {
     public static void main(String[] args) {
         System.out.println("second commit");
         System.out.println("thrid commit");
+        System.out.println("hot-fix");
     }
 }
