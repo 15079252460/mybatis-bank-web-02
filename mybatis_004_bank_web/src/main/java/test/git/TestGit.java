6a2e6153d6b0e28d0807e4566610c38854c48355
liangfengcai <1183268720@qq.com>
2022-11-09 16:41:35 +0800

    thrid commit

--- mybatis_004_bank_web/src/main/java/test/git/TestGit.java
+++ mybatis_004_bank_web/src/main/java/test/git/TestGit.java
@@ -3,5 +3,6 @@ package test.git;
 public class TestGit {
     public static void main(String[] args) {
         System.out.println("second commit");
+        System.out.println("thrid commit");
     }
 }
